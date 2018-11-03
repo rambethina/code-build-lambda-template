b4be927d8139e5836ad763d52de69247be08c55c
vijayaram bethina <vijayarambethina@vijayarams-Mac-mini.local>
2018-11-03 15:24:02 -0500

    added version text

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
     // TODO implement
+    console.log('Version 1.0 of testing code build');
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
